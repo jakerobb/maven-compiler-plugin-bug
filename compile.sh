65397c6a3fcdb3fe7924e5ab28f453494fe638ad
jrobb <jrobb@vertafore.com>
2019-10-08 10:57:43 -0400

    adding .gitignore and checking in compile.sh

---
+++ compile.sh
@@ -0,0 +1,1 @@
+javac -d project-to-compile/out -sourcepath project-to-compile/src: project-to-compile/src/PointlessIntegerBuilder.java project-to-compile/src/ThisWillFailToCompile.java -target 12 -source 12
